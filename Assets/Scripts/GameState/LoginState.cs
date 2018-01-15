a4ee95b9ea4a3b17db5124bdd20571f62365db7a
EvilPomelo <123227682@qq.com>
2018-01-15 14:16:11 +0800

    init

---
+++ Assets/Scripts/GameState/LoginState.cs
@@ -0,0 +1,18 @@
+﻿
+    public class LoginState:GameState
+    {
+        protected override void OnStart()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnStop()
+        {
+            GUIManager.HideView("LoginPanel");
+        }
+
+        protected override void OnLoadComplete()
+        {
+            GUIManager.ShowView("LoginPanel");
+        }
+    }
