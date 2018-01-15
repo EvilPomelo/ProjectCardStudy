cd1d76bf2a94413ff484d3b72285b72521330356
EvilPomelo <123227682@qq.com>
2018-01-15 18:13:17 +0800

    18:13 save

--- Assets/Scripts/ResourcesManager/ResourcesManager.cs
+++ Assets/Scripts/ResourcesManager/ResourcesManager.cs
@@ -1,5 +1,36 @@
-﻿
-    public class ResourcesManager:Monobeha
+﻿using UnityEngine;
+public class ResourcesManager:MonoBehaviour
+{
+    private static ResourcesManager _Instace = null;
+
+    public static ResourcesManager Instance
     {
-        
+        get
+        {
+            if (_Instace==null)
+            {
+                _Instace=new ResourcesManager();
+            }
+            return _Instace;
+        }
     }
+
+    private string uiPanelPath = "UI/Panel";
+
+    public GameObject GetUIPrefab(string name)
+    {
+        return LoadPrefab(name,uiPanelPath);
+    }
+
+    public GameObject LoadPrefab(string name,string path)
+    {
+        string loadPath = path + "/" + name;
+        GameObject prefab=Resources.Load(loadPath,typeof(GameObject)) as GameObject;
+        if (prefab==null)
+        {
+            Debug.LogError("prefab is null,loadPath" +loadPath);
+        }
+
+        return prefab;
+    }
+}
